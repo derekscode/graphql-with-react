2ebe1bacb02528e1925d527b39d571077a85f32c
derekscode <17056283+derekscode@users.noreply.github.com>
2020-09-15 10:40:08 -0700

    fixed

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const expressGraphQL = require("express-graphql").graphqlHTTP;
+const expressGraphQL = require("express-graphql");
 const schema = require("./schema/schema");
 
 const app = express();
